83e8ee16babccfbdd9f80dcf212c3fd117c88fe4
Julian Krustev <juliankrustev2018@gmail.com>
2022-12-03 18:31:42 +0200

    Added Sidebar component and navigation for mobile devices

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -28,7 +28,6 @@ const Login = () => {
 
     const responseGoogle = (response) => {
         localStorage.setItem('user', JSON.stringify(response.profileObj));
-        console.log(response)
         const { name, googleId, imageUrl } = response.profileObj;
 
         const doc = {
